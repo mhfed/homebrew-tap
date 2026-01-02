713c70efdae8048a87f22f82030a2a2405004173
mhfed <nmhieu04091999@gmail.com>
2026-01-02 13:23:27 +0700

    first commit

--- Formula/safe-rm-hehe.rb
+++ Formula/safe-rm-hehe.rb
@@ -2,7 +2,7 @@ class SafeRmHehe < Formula
   desc "🛡️ Vị cứu tinh cho Dev - Chuyển rm thành Trash"
   homepage "https://github.com/mhfed/safe-rm-hehe"
   url "https://github.com/mhfed/safe-rm-hehe/archive/refs/tags/v0.1.2.tar.gz"
-  version "0.1.0" # <--- THÊM DÒNG NÀY ĐỂ FIX LỖI NIL VERSION
+  version "0.1.2" # <--- THÊM DÒNG NÀY ĐỂ FIX LỖI NIL VERSION
   sha256 "DÁN_MÃ_SHA256_CỦA_BẠN_VÀO_ĐÂY"
   license "MIT"
 
